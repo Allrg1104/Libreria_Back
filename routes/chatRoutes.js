b791a1f6167c8c33a2edacc921e76c8d3fa660cc
Rivera G Alejandro <alejandro110498@hotmail.com>
2025-08-25 14:06:44 -0500

    Update chatRoutes.js

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -8,7 +8,7 @@ router.post('/newUser', createUser);
 router.get('/usuarios', getUsuario);
 router.post('/logout', logoutUser);
 router.post('/newventa', createVenta);
-router.post('/ventas', getVenta);
+router.post('/ventas', listarVentas);
 
 router.post('/', generateChatResponse);
 router.get('/history/:userId', getConversationHistory);
